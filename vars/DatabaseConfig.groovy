a4af5c0b0ff9f2f56c88fa1e4b7d7b02c04a116d
mrjraviteja <mrjraviteja333@gmail.com>
2024-06-04 11:25:20 +0530

    Added DatabaseConfig

---
+++ vars/DatabaseConfig.groovy
@@ -0,0 +1,19 @@
+def getDatabaseConfig(String domain) {
+    def dbUrl
+    def dbUser
+    def dbPassword
+
+    if (domain == "effigotesting1") {
+        dbUrl = "jdbc:postgresql://localhost:5432/effigotesting1"
+        dbUser = "postgres"
+        dbPassword = "sillicon"
+    } else if (domain == "effigotesting2") {
+        dbUrl = "jdbc:postgresql://localhost:5432/effigotesting2"
+        dbUser = "postgres"
+        dbPassword = "sillicon"
+    } else {
+        return null
+    }
+
+    return [dbUrl: dbUrl, dbUser: dbUser, dbPassword: dbPassword]
+}
